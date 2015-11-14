f9ccaa6c273a609cf0ec483b49eb282cd67f0da1
180Tobogun <andreykoroka@gmail.com>
2015-11-14 20:23:28 +0200

    fix phpunit

--- src/Tobogun/HomepageBundle/Tests/Controller/HomepageControllerTest.php
+++ src/Tobogun/HomepageBundle/Tests/Controller/HomepageControllerTest.php
@@ -4,14 +4,17 @@ namespace Tobogun\HomepageBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class DefaultControllerTest
+ */
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("ICabbage")')->count() > 0);
     }
 }
